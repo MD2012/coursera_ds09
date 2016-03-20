f26ddca241e359ecdaad988ff482bc0569696406
dcon2016 <md@doulis.com>
2016-03-20 23:42:50 +0100

    v3

--- cproj_nasdaq/server.R
+++ cproj_nasdaq/server.R
@@ -1,8 +1,19 @@
 library(shiny)
+library(xts)
+library(dygraphs)
+
+ndq_data_url = "http://real-chart.finance.yahoo.com/table.csv?s=%5EIXIC&d=2&e=20&f=2016&g=d&a=1&b=5&c=1971&ignore=.csv"
+ndq_data = read.csv(ndq_data_url)
+ndq_data = ndq_data[ ,c('Date', 'Close')]
+head(ndq_data)
+ndq_xts = xts(ndq_data[,-1],order.by=as.POSIXct(ndq_data$Date))
+
+ret<-diff(log(ndq_data$Close))
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
+ 
   # Expression that generates a histogram. The expression is
   # wrapped in a call to renderPlot to indicate that:
   #
@@ -11,13 +22,21 @@ shinyServer(function(input, output) {
   #  2) Its output type is a plot
   
 
-
-  output$histPlot <- renderPlot({
-    x    <- faithful[, 2]  # Old Faithful Geyser data
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  output$header <- renderText({ 
+    paste("You selected: ", input$select)
+  })
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  output$pricesPlot <- renderPlot({ 
+    dygraph(ndq_xts)
+  })
+  output$pricesPlot <- renderDygraph({
+    dygraph(ndq_xts, main="Nasdaq Prices Timeseries")
   })
   
+  output$returnsPlot <- renderPlot({ 
+    hist(ret, probability = TRUE, breaks = as.numeric(input$bins),
+         xlab = "Log Return", main = "Nasdaq Log Return Distribution", 
+         col = 'blue', border = 'white')
+  })
+      
 })
--- cproj_nasdaq/ui.R
+++ cproj_nasdaq/ui.R
@@ -9,16 +9,63 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number histogram bins",
-                  min = 10,
-                  max = 50,
-                  value = 30)
+      selectInput("select", label = "Display", 
+                  choices = list("Nasdaq Prices" = "Nasdaq Prices", "Nasdaq Returns" = "Nasdaq Returns"), selected = 1),
+      
+      conditionalPanel(
+        condition = "input.select == 'Nasdaq Prices'"
+      ),
+      conditionalPanel(
+        condition = "input.select == 'Nasdaq Returns'",
+        
+        sliderInput("bins",
+                    "Number histogram bins",
+                    min = 30,
+                    max = 90,
+                    value = 60)
+      ),
+      
+      conditionalPanel(
+        condition = "input.select == 'Nasdaq Prices'",
+        
+        p("First we look at the NASDAQ Composite Index prices as obtained from finance.yahoo.com"),
+        code("ndq_data_url = http://real-chart.finance.yahoo.com/table.csv
+              ?s=%5EIXIC&d=2&e=20
+              &f=2016&g=d&a=1&b=5&c=1971&ignore=.csv
+              ndq_data = read.csv(ndq_data_url)"),
+        br(),
+        p("The chart we produce (conditionally if 'Nasdaq Prices' Select box option is selected) 
+          is based on the dygraphs library")
+        
+      ),
+      
+      conditionalPanel(
+        condition = "input.select == 'Nasdaq Returns'",
+        
+        br(),
+        p("Now that you selected 'Nasdaq Returns' we display the returns' distribution plot 
+          of the returns of the loaded Nasdaq Price Timeseries as calculated in server.R."),
+        br(),
+        p("Using the Sliderbar above, we can interactively change the number of bins used in the histogram.")
+      )
+      
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("histPlot")
+      
+      h4(textOutput("header")),
+      
+      conditionalPanel(
+        condition = "input.select == 'Nasdaq Prices'",
+          dygraphOutput("pricesPlot")
+      ),
+      
+      conditionalPanel(
+        condition = "input.select == 'Nasdaq Returns'",
+        plotOutput("returnsPlot")
+      )
+      
     )
     
   )
